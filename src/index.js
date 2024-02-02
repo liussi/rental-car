cce6abe444046d178a15baffb6e43702a270c80a
liussi <125599487+liussi@users.noreply.github.com>
2024-02-02 22:29:33 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
 
-    <BrowserRouter >
+    <BrowserRouter  basename="/car-for-rent">
       <App />
       </BrowserRouter>
        </Provider>
