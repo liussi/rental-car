341ab3b3e57fc89c5f9d41dca05a7fbd59a7d344
liussi <125599487+liussi@users.noreply.github.com>
2024-03-04 11:28:34 +0200

    fix name

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import GlobalStyle from 'styles/globalStyles';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-<BrowserRouter basename="/cars-for-rent">
+<BrowserRouter basename="/rental-car">
   <GlobalStyle />
       <App />
       </BrowserRouter>
